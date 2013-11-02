4a60b9125348c0023f1ce89ed519b0f6521a94e0
Joao Pescada <joao.pescada@gmail.com>
2013-11-02 00:22:00 +0000

    Fixed issue of notifications not showing in Android when app is closed (issue #85)

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -69,8 +69,8 @@ public class GCMIntentService extends GCMBaseIntentService {
 		{
 			PushPlugin.sendExtras(extras);
 
-			// Send a notification if there is a message and not in foreground
-			if (!PushPlugin.isInForeground() && extras.getString("message").length() != 0) {
+			// Send a notification if there is a message and not in foreground or webview not active (app closed)
+			if ( (!PushPlugin.isInForeground() || !PushPlugin.isActive()) && extras.getString("message").length() != 0) {
 				createNotification(context, extras);
 			}
 		}
